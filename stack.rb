d00ad0a789538b7be24129ba55ee2efe61730f12
tra38 <tra38@nau.edu>
2016-03-05 22:37:09 -0600

    Add ArrayList version of Stacks

--- stack.rb
+++ stack.rb
@@ -1,26 +1,61 @@
 require_relative 'linked_list'
 
 # Linked List Version
-class Stack
-  attr_reader :stack
+# class Stack
+#   attr_reader :stack
 
+#   def initialize
+#     @stack = LinkedList.new
+#   end
+
+#   def top
+#     node = stack.get(0)
+#     reveal_node(node)
+#   end
+
+#   def push(element)
+#     node = Node.new(element)
+#     stack.insert_first(node)
+#   end
+
+#   def pop
+#     node = stack.remove_first
+#     reveal_node(node)
+#   end
+
+#   def empty?
+#     self.top == nil
+#   end
+
+#   private
+#   def reveal_node(node)
+#     if node
+#       node.element
+#     else
+#       nil
+#     end
+#   end
+
+# end
+
+#Array List Version
+class Stack
   def initialize
-    @stack = LinkedList.new
+    @stack = ArrayList.new(5)
   end
 
   def top
-    node = stack.get(0)
-    reveal_node(node)
+    @stack.get(0)
   end
 
   def push(element)
-    node = Node.new(element)
-    stack.insert_first(node)
+    @stack.insert(0,element)
   end
 
   def pop
-    node = stack.remove_first
-    reveal_node(node)
+    value = self.top
+    create_new_array_list
+    value
   end
 
   def empty?
@@ -28,12 +63,13 @@ class Stack
   end
 
   private
-  def reveal_node(node)
-    if node
-      node.element
-    else
-      nil
+  def create_new_array_list
+    old_stack_length = @stack.size
+    new_stack = ArrayList.new(5)
+    old_stack_length.times do |index|
+      old_value = @stack.get(index + 1)
+      new_stack.set(index, old_value)
     end
+    @stack = new_stack
   end
-
 end
