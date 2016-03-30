9f4796cff46fd718b55b450073063da379c12a8b
tra38 <tra38@nau.edu>
2016-03-30 16:09:31 -0500

    Add sets

---
+++ set.rb
@@ -0,0 +1,68 @@
+require_relative 'array_list'
+
+class MySet
+  attr_reader :entries
+  def initialize
+    @entries = ArrayList.new(5)
+  end
+
+  def size
+    entries.size
+  end
+
+  def contains?(element)
+    index_position = element_search(element)
+    if index_position
+      return true
+    else
+      return false
+    end
+  end
+
+  def add(element)
+    entries.add(element)
+  end
+
+  def remove(element)
+    index_position = element_search(element)
+    if index_position
+      # @size -= 1
+      entries.set(index_position, nil)
+      compact_entries
+    else
+      false
+    end
+  end
+
+  def iterate(&block)
+    size.times do |index|
+      element = entries.get(index)
+      block.call(element)
+    end
+  end
+
+  private
+  def element_search(element)
+    size.times do |index|
+      if entries.get(index) == element
+        return index
+      end
+    end
+    false
+  end
+
+  def compact_entries
+    temp_entries = ArrayList.new(5)
+    counter = 0
+    size.times do |index|
+      element = entries.get(index)
+      if element == nil
+        next
+      else
+        temp_entries.set(counter, element)
+      end
+    end
+    @entries = temp_entries
+  end
+
+end
---
+++ spec/set_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../set'
+
+RSpec.describe "MySet" do
+  before(:each) do
+    @set = MySet.new
+    @word = "cough"
+  end
+
+  it "starts off as an empty set" do
+    expect(@set.size).to eq(0)
+    expect(@set.contains?(@word)).to eq(false)
+  end
+
+  it "add an element to the set" do
+    @set.add(@word)
+    expect(@set.size).to eq(1)
+    expect(@set.contains?(@word)).to eq(true)
+  end
+
+  it "can remove an element from the set" do
+    word = "cough"
+    @set.add(@word)
+    @set.remove(@word)
+    expect(@set.size).to eq(0)
+    expect(@set.contains?(word)).to eq(false)
+  end
+
+  it "can iterate through a Set" do
+    @set.add(1)
+    @set.add(2)
+    @set.add(3)
+    sum = 0
+    @set.iterate { |number| sum += number }
+    expect(sum).to eq(6)
+  end
+
+end
